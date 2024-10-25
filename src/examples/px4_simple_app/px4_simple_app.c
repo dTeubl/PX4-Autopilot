10947dd03f4284c86770a36eff451ff0ee05abf2
Daniel Teubl <teubl.dani@gmail.com>
2024-10-25 12:18:58 +0200

    Small update in sample app, JETI module started

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -38,24 +38,23 @@
  * @author Example User <mail@example.com>
  */
 
-#include <px4_platform_common/px4_config.h>
-#include <px4_platform_common/log.h>
-#include <px4_platform_common/tasks.h>
-#include <px4_platform_common/posix.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <poll.h>
-#include <string.h>
 #include <math.h>
+#include <poll.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/tasks.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-#include <uORB/uORB.h>
 #include <uORB/topics/vehicle_acceleration.h>
 #include <uORB/topics/vehicle_attitude.h>
+#include <uORB/uORB.h>
 
 __EXPORT int px4_simple_app_main(int argc, char *argv[]);
 
-int px4_simple_app_main(int argc, char *argv[])
-{
+int px4_simple_app_main(int argc, char *argv[]) {
 	PX4_INFO("Hello Sky!");
 
 	/* subscribe to vehicle_acceleration topic */
@@ -68,30 +67,38 @@ int px4_simple_app_main(int argc, char *argv[])
 	memset(&att, 0, sizeof(att));
 	orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
 
-	/* one could wait for multiple topics with this technique, just using one here */
+	/* one could wait for multiple topics with this technique, just using
+	 * one here */
 	px4_pollfd_struct_t fds[] = {
-		{ .fd = sensor_sub_fd,   .events = POLLIN },
-		/* there could be more file descriptors here, in the form like:
-		 * { .fd = other_sub_fd,   .events = POLLIN },
-		 */
+	    {.fd = sensor_sub_fd, .events = POLLIN},
+	    /* there could be more file descriptors here, in the form like:
+	     * { .fd = other_sub_fd,   .events = POLLIN },
+	     */
 	};
 
 	int error_counter = 0;
 
-	for (int i = 0; i < 5; i++) {
-		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+	for (int i = 0; i < 10; i++) {
+		/* wait for sensor update of 1 file descriptor for 1000 ms (1
+		 * second) */
 		int poll_ret = px4_poll(fds, 1, 1000);
 
 		/* handle the poll result */
 		if (poll_ret == 0) {
 			/* this means none of our providers is giving us data */
 			PX4_ERR("Got no data within a second");
+			continue;
+		}
 
-		} else if (poll_ret < 0) {
-			/* this is seriously bad - should be an emergency */
+		if (poll_ret < 0) {
+			/* this is seriously bad - should be an
+			 * emergency */
 			if (error_counter < 10 || error_counter % 50 == 0) {
-				/* use a counter to prevent flooding (and slowing us down) */
-				PX4_ERR("ERROR return value from poll(): %d", poll_ret);
+				/* use a counter to prevent flooding
+				 * (and slowing us down) */
+				PX4_ERR("ERROR return value from "
+					"poll(): %d",
+					poll_ret);
 			}
 
 			error_counter++;
@@ -99,27 +106,35 @@ int px4_simple_app_main(int argc, char *argv[])
 		} else {
 
 			if (fds[0].revents & POLLIN) {
-				/* obtained data for the first file descriptor */
+				/* obtained data for the first file
+				 * descriptor
+				 */
 				struct vehicle_acceleration_s accel;
-				/* copy sensors raw data into local buffer */
-				orb_copy(ORB_ID(vehicle_acceleration), sensor_sub_fd, &accel);
-				PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f",
-					 (double)accel.xyz[0],
+				/* copy sensors raw data into local
+				 * buffer */
+				orb_copy(ORB_ID(vehicle_acceleration),
+					 sensor_sub_fd, &accel);
+				PX4_INFO("Accelerometer:\t%d:\t%8.4f\t%8."
+					 "4f\t%8.4f",
+					 (unsigned int)i, (double)accel.xyz[0],
 					 (double)accel.xyz[1],
 					 (double)accel.xyz[2]);
 
-				/* set att and publish this information for other apps
-				 the following does not have any meaning, it's just an example
+				/* set att and publish this information
+				 for other apps the following does not
+				 have any meaning, it's just an example
 				*/
 				att.q[0] = accel.xyz[0];
 				att.q[1] = accel.xyz[1];
 				att.q[2] = accel.xyz[2];
 
-				orb_publish(ORB_ID(vehicle_attitude), att_pub, &att);
+				orb_publish(ORB_ID(vehicle_attitude), att_pub,
+					    &att);
 			}
 
-			/* there could be more file descriptors here, in the form like:
-			 * if (fds[1..n].revents & POLLIN) {}
+			/* there could be more file descriptors here, in
+			 * the form like: if (fds[1..n].revents &
+			 * POLLIN) {}
 			 */
 		}
 	}
