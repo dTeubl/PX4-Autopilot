6d9bb453131925bef203c9104950767a952a664d
Daniel Teubl <teubl.dani@gmail.com>
2024-10-25 16:26:45 +0200

    Unit test attached for JETI. TDD can be started

---
+++ src/examples/jeti_extbus/JetiTest.cpp
@@ -0,0 +1,76 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2019-2024 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include <cstdint>
+#include <gtest/gtest.h>
+
+/**
+ * Basic unit tests to drive the core functionality
+ * of the JETI ext protokol reception side
+ */
+
+/**
+ * A sample msg from data sheet, page 6.
+ *
+ * Macket From Maset
+ *
+ * 0x3E 0x03 0x28 0x06 0x31 0x20 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x82
+ * 0x1F 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x82 0x1F
+ * 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x82 0x1F 0x4F 0xE2
+ *
+ * 0x3E 0x03 – Packet header that forbids answering
+ * 0x28 – Message length (40)
+ * 0x06 – Packet ID 0x31 – The data identifier – channel values
+ * 0x20 – Length of data blocks (32) - 16 channels x 2B
+ * 0x1F82 – Value of the 1st channel (8066)/8000000 = 1,00825ms
+ *
+ * 0xE24F - CRC16-CCITT
+ *
+ */
+
+TEST(JETIStatus, Probe) { EXPECT_FALSE(false); }
+
+class JETIChannelData : public testing::Test {
+      protected:
+	JETIChannelData() {}
+
+	uint8_t raw_data[40] = {
+	    0x3E, 0x03, 0x28, 0x06, 0x31, 0x20, 0x82, 0x1F, 0x82, 0x1F,
+	    0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F,
+	    0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F,
+	    0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F, 0x82, 0x1F, 0x4F, 0xE2,
+	};
+};
+
+TEST_F(JETIChannelData, Probe) { EXPECT_EQ(raw_data[0], 0x3E); }
+
